2eed4a215733a63a7ea89a391ca32daef8f9dd59
pratikgupta190804 <pratikgupta040819@gmail.com>
2024-09-07 14:04:34 +0530

    Implement Searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -23,10 +23,11 @@ const useGames = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
       },
     },
-    [gameQuery.genre?.id, gameQuery.platform?.id, gameQuery.sortOrder]
+    [gameQuery]
   );
 
 export default useGames;
